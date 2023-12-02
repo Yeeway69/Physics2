14c958f62a030cd2b5dcc0e4854eff4d4664e9f8
Alex Becerra Garcia <abecerragarcia04@gmail.com>
2023-12-02 17:13:19 +0100

    second player done

--- WormGameEngine/Physics II - 2D Project Clean Template/ModulePlayer.cpp
+++ WormGameEngine/Physics II - 2D Project Clean Template/ModulePlayer.cpp
@@ -31,6 +31,17 @@ bool ModulePlayer::Start()
 	cannonAngle = 45.0f;  // Default to 45 degrees
 	cannonPower = 60.0f;   // Default power
 	LOG("Loading player");
+
+	//player2
+	player2->position = { 950.0f,SCREEN_HEIGHT - 300 };
+	player2->velocity = { 0.0f, 0.0f }; // starting velocity
+	player2->acceleration = gravity; // affected by gravity
+	player2->mass = 1.0f; // arbitrary mass
+	player2->elasticity = 0.5f; // some bounce
+	player2->friction = 0.1f; // some friction
+	cannonAngle2 = 135.0f;  // Default to 45 degrees
+	cannonPower2 = 60.0f;   // Default power
+	app->physics->bodies.push_back(player2);
 	return true;
 }
 
@@ -46,41 +57,87 @@ bool ModulePlayer::CleanUp()
 update_status ModulePlayer::Update()
 {
 	// Hotkey to adjuct cannon ball shooting direction
-	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-	{
-		cannonAngle += 1.0f;
+
+	if (firstplayer == true) {
+		if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		{
+				cannonAngle += 1.0f;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		{
+			cannonAngle -= 1.0f;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT && cannonPower < 150)  // Adjust as needed based on your input handling
+		{
+				cannonPower += 0.5f;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT && cannonPower > 0)  // Adjust as needed based on your input handling
+		{
+				cannonPower -= 0.5f;
+		}
 	}
-	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-	{
-		cannonAngle -= 1.0f;
+	else {
+		if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		{
+			cannonAngle2 += 1.0f;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		{
+			cannonAngle2 -= 1.0f;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT && cannonPower2 < 150)  // Adjust as needed based on your input handling
+		{
+			cannonPower2 += 0.5f;
+		}
+		if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT && cannonPower2 > 0)  // Adjust as needed based on your input handling
+		{
+			cannonPower2 -= 0.5f;
+		}
 	}
-	if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT && cannonPower<150)  // Adjust as needed based on your input handling
-	{
-		cannonPower += 0.5f;
-	}
-	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT && cannonPower>0)  // Adjust as needed based on your input handling
-	{
-		cannonPower -= 0.5f;
-	}
+	
 
 	const fPoint cannonPosition = { player->position.x, player->position.y - 10 };
+	const fPoint cannonPosition2 = { player2->position.x, player2->position.y - 10 };
+	
 
 
 	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN &&!hasShot) // Listen for SPACEBAR press
 	{
-		hasShot = true;
-		Body* cannonball = new Body();
-		cannonball->position = cannonPosition;
-		cannonball->velocity = { -2.0f, -4.0f }; // Initial velocity (adjust as needed)
-		cannonball->acceleration = gravity; // Affected by gravity
-		cannonball->mass = 1.0f; // Arbitrary mass
-		cannonball->elasticity = 0.5f; // Some bounce
-		cannonball->friction = 0.1f; // Some friction
-		app->physics->bodies.push_back(cannonball);
+		if (firstplayer == true) {
+			hasShot = true;
+			Body* cannonball = new Body();
 
-		float radianAngle = DEGTORAD(cannonAngle);  // Convert angle  to radians
-		cannonball->velocity.x = cannonPower * cosf(radianAngle);
-		cannonball->velocity.y = -cannonPower * sinf(radianAngle);  // Negative because y is up
+			cannonball->position = cannonPosition;
+			cannonball->velocity = { -2.0f, -4.0f }; // Initial velocity (adjust as needed)
+			cannonball->acceleration = gravity; // Affected by gravity
+			cannonball->mass = 1.0f; // Arbitrary mass
+			cannonball->elasticity = 0.5f; // Some bounce
+			cannonball->friction = 0.1f; // Some friction
+			app->physics->bodies.push_back(cannonball);
+
+			float radianAngle = DEGTORAD(cannonAngle);  // Convert angle  to radians
+			cannonball->velocity.x = cannonPower * cosf(radianAngle);
+			cannonball->velocity.y = -cannonPower * sinf(radianAngle);  // Negative because y is up
+			firstplayer = false;
+		}
+		else {
+			Body* cannonball2 = new Body();
+			cannonball2->position = cannonPosition2;
+			cannonball2->velocity = { -2.0f, -4.0f }; // Initial velocity (adjust as needed)
+			cannonball2->acceleration = gravity; // Affected by gravity
+			cannonball2->mass = 1.0f; // Arbitrary mass
+			cannonball2->elasticity = 0.5f; // Some bounce
+			cannonball2->friction = 0.1f; // Some friction
+			app->physics->bodies.push_back(cannonball2);
+
+			float radianAngle = DEGTORAD(cannonAngle2);  // Convert angle  to radians
+			cannonball2->velocity.x = cannonPower2 * cosf(radianAngle);
+			cannonball2->velocity.y = -cannonPower2 * sinf(radianAngle);  // Negative because y is up
+			firstplayer = true;
+		}
+		
+
+		
 
 	}
 
@@ -95,15 +152,26 @@ update_status ModulePlayer::Update()
 	}
 	else
 	{
-		float radianAngle = DEGTORAD(-cannonAngle);
-		int endX = player->position.x + static_cast<int>(100 * std::cosf(radianAngle));
-		int endY = player->position.y + static_cast<int>(100 * std::sinf(radianAngle));
+		if (firstplayer == true) {
+			float radianAngle = DEGTORAD(-cannonAngle);
+			int endX = player->position.x + static_cast<int>(100 * std::cosf(radianAngle));
+			int endY = player->position.y + static_cast<int>(100 * std::sinf(radianAngle));
 
-		App->renderer->DrawLine(endX, endY, player->position.x, player->position.y, 255, 255, 255);
+			App->renderer->DrawLine(endX, endY, player->position.x, player->position.y, 255, 255, 255);
+		}
+		else {
+			float radianAngle = DEGTORAD(-cannonAngle2);
+			int endX = player2->position.x + static_cast<int>(100 * std::cosf(radianAngle));
+			int endY = player2->position.y + static_cast<int>(100 * std::sinf(radianAngle));
+
+			App->renderer->DrawLine(endX, endY, player2->position.x, player2->position.y, 255, 255, 255);
+		}
+		
 	}
 
 	//This is the power var 
 	App->renderer->DrawGrowingRectangle(SCREEN_WIDTH*0.05f, SCREEN_HEIGHT/2, cannonPower, 255, 255, 255,255, true);
+	App->renderer->DrawGrowingRectangle(SCREEN_WIDTH * 0.05f, SCREEN_HEIGHT / 2, cannonPower2, 255, 255, 255, 255, true);
 
 
 
